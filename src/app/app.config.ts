b46fc2248e557f019e83c0b0cd9a348f85a47ec7
Ahmed Atef <Ahmedatef32003@gmail.com>
2025-06-27 00:31:55 +0300

    fix primng Icon

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withHashLocation, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes ,withHashLocation() ,withViewTransitions() , withInMemoryScrolling()),
+     provideClientHydration(withEventReplay()),
         provideHttpClient(withFetch())
     ]
-};
+}
